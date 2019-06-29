74272c1808d2172b9b0fe77814142bb0c58f8a47
subodhss23@gmail.com <subodhss23@gmail.com>
2019-06-29 01:10:48 -0700

    event handling

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ var app = new Vue({
     el: "#app",
     data: {
         product: 'Socks',
-        image: './socks.jpg',
+        image: './socks_green.jpg',
         link: 'https://www.google.com',
         inStock: true,
         details: [
             "80% cotton", "20% polyster", "Gender-neutral"
         ],
-        sizes: [
-            "small", "medium", "large", "extra-large"
+        cart: 0,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: './socks_green.jpg'
+            },
+            {
+                varinatId: 2245,
+                variantColor: 'blue',
+                variantImage: './socks_blue.jpg'
+            }
         ]
+    },
+    methods:{ 
+        addToCart(){
+            this.cart += 1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage;
+        }
     }
+    
 })
