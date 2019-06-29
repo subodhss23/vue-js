922f09ba1c4e79db58e5c59a6ca473b74394ec1a
subodhss23@gmail.com <subodhss23@gmail.com>
2019-06-29 00:23:18 -0700

    v-if-else: conditional rendering

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
         product: 'Socks',
         image: './socks.jpg',
         link: 'https://www.google.com',
-        description: 'do not buy this socks'
+        description: 'do not buy this socks',
+        inventory: 0
     }
 })
