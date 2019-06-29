cb7c2eb4ffc93c979d880951a5ba464356f269ee
subodhss23@gmail.com <subodhss23@gmail.com>
2019-06-28 23:54:28 -0700

    v-bind, binding data object

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        product: 'Socks',
+        image: './socks.jpg',
+        link: 'https://www.google.com',
+        description: 'do not buy this socks'
+    }
+})
